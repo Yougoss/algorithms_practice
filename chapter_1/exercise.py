d9243e9a20be5fb9311f40495f00d9c423d446d0
xly <171538166@qq.com>
2017-02-10 18:27:26 +0800

    merge_sort unfinished

---
+++ chapter_1/exercise.py
@@ -0,0 +1,29 @@
+# coding:utf-8
+# diff = 0
+# for n in range(10000):
+#     a = n * n
+#     b = pow(2, n)
+#     if (a - b) * diff <= 0:
+#         print n, a, b, diff
+#     diff = a-b
+
+# 1-1
+# sec = 1
+# min = sec * 60
+# hour = min * 60
+# day = hour * 24
+# month = day * 30
+# year = day * 365
+# century = year * 100
+#
+# times = [sec, min, hour, day, month]
+# for t in times:
+#     print pow(2, t)
+#
+#     print t*t
+#
+#     print t
+
+
+
+
